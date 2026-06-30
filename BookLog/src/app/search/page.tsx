250325f2a11c9512bd33b03a749e4fd7f031e3cd
hy <62981058+hhy9@users.noreply.github.com>
2026-06-30 09:16:34 +0900

    Delete BookLog directory

--- BookLog/src/app/search/page.tsx
+++
@@ -1,5 +0,0 @@
-import { SearchPage } from "@/features/search/search-page";
-
-export default function Page() {
-  return <SearchPage />;
-}
--- BookLog/src/app/shelf/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ShelfPage } from "@/features/shelf/shelf-page";
-
-export default function Page() {
-  return <ShelfPage />;
-}
